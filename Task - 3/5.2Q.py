7055d10b4f1f453866d22926f3863199f6f62dd5
sainithin-99 <75233578+sainithin-99@users.noreply.github.com>
2022-06-19 23:35:20 +0530

    Update 5.2Q.py

--- Task - 3/5.2Q.py
+++ Task - 3/5.2Q.py
@@ -1,20 +1,13 @@
-A = [[12, 17, 3],
+import numpy
+
+matrix_1 = [[12, 17, 3],
     [4, 5, 6],
     [7, 8, 9]]
 
-B = [[5, 18, 1, 2],
+matrix_2 = [[5, 18, 1, 2],
     [6, 7, 3, 0],
     [4, 5, 9, 1]]
-	
-result = [[0, 0, 0, 0],
-	[0, 0, 0, 0],
-	[0, 0, 0, 0]]
 
+result = numpy.dot(matrix_1, matrix_2)
 
-for i in range(len(A)):
-	for j in range(len(B[0])):
-		for k in range(len(B)):
-			result[i][j] += A[i][k] * B[k][j]
-
-for r in result:
-	print(r)
+print(result)
